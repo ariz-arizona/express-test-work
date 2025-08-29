d477f21ae9a8aaa24e685f835c1f909e0e3feb86
aarizona <ar.ariz.arizona@gmail.com>
2025-08-29 17:07:31 +0300

    base url

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -17,6 +17,9 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  app: {
+    baseURL: '/et'
+  },
   runtimeConfig: {
     apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
     public: {
